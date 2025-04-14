5c877aaccb1a81debde86c716246e3be453704a8
Pedro Martins <pedrofmartins2004@gmail.com>
2025-04-14 01:47:09 +0100

    parte 2 notificacao

--- cidade-mais/src/services/respostaTicket.service.js
+++ cidade-mais/src/services/respostaTicket.service.js
@@ -1,8 +1,18 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient, tipo_notificacao } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 // Criar resposta
 async function createResposta(data) {
+
+    const novaResposta = await prisma.resposta_ticket.create({ data });
+    notificacao = novaResposta.estado_resposta == 'resolvido' ? tipo_notificacao.Sucesso : tipo_notificacao.Insucesso
+    // 2. Cria a notificação usando dados do post criado
+    await createNotificacao({
+        id_utilizador: novaResposta.id_utilizador,
+        id_ticket: novaResposta.id_ticket,
+        tipo_notificacao: notificacao
+    });
+
     return prisma.resposta_ticket.create({ data });
 }
 
