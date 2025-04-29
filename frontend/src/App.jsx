9fcfc4ae116f7387256d610a32b0fb07c08a73e4
Hugo Azevedo <hugoafazevedo17@gmail.com>
2025-04-29 19:32:33 +0100

    Corrigir Css enviar ticket

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,8 +5,9 @@ import PublicacoesETickets from './pages/PublicacoesETicketsUtilizador';
 import Login from './pages/Login';
 import Registar1 from './pages/Registar1';
 import Registar2 from './pages/Registar2';
-import './App.css';
 import EnviarTicket from './pages/EnviarTicket';
+import './App.css';
+
 
 
 function App() {
@@ -19,7 +20,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/registar1" element={<Registar1 />} />
         <Route path="/registar2" element={<Registar2 />} />
-        <Route path="/pages/EnviarTicket" element={<EnviarTicket />} />
+        <Route path="/enviar-ticket" element={<EnviarTicket />} />
       </Routes>
     </Router>
   );
--- frontend/src/pages/EnviarTicket.jsx
+++ frontend/src/pages/EnviarTicket.jsx
@@ -1,11 +1,17 @@
-import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import BarraSuperior from '../componentes/BarraSuperior';
 import BarraLateral from '../componentes/BarraLateral';
-import '../styles/CriarFreguesia.css';
+import '../styles/EnviarTicket.css';
 
 function EnviarTicket() {
   const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // lógica para enviar dados
+    alert('Ticket enviado!');
+    navigate('/dashboard');
+  };
 
   return (
     <div className="container">
@@ -13,16 +19,14 @@ function EnviarTicket() {
       <div className="corpo">
         <BarraLateral />
         <div className="conteudo-criar">
-          <h2>Descrição de Problemers</h2>
+          <h2>Ticket</h2>
           <hr />
-
-            <label htmlFor="localizacao">Localização</label>
-            <input type="text" id="localizacao" placeholder="Localização" required />
-
+          <form className="formulario" onSubmit={handleSubmit}>
             <label htmlFor="descricao">Descrição</label>
             <textarea id="descricao" placeholder="Descrição" rows="5" required></textarea>
 
             <button type="submit" className="btn-enviar">Enviar</button>
+          </form>
         </div>
       </div>
     </div>
