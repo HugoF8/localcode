7f61051c0febc73f9b545c5b4be9928b97ff8174
Mateus <mateus.alves.quintela@gmail.com>
2025-04-29 18:34:14 +0100

    enviartickets, nao verificado

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home';
 import CriarFreguesia from './pages/CriarFreguesia';
 import PublicacoesETickets from './pages/PublicacoesETicketsUtilizador';
 import './App.css';
+import EnviarTicket from './pages/EnviarTicket';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/criar-freguesia" element={<CriarFreguesia />} />
         <Route path="/publicacoes-tickets-utilizador" element={<PublicacoesETickets />} />
+        <Route path="/pages/EnviarTicket" element={<EnviarTicket />} />
       </Routes>
     </Router>
   );
--- frontend/src/pages/EnviarTicket.jsx
+++ frontend/src/pages/EnviarTicket.jsx
@@ -1,1 +1,32 @@
-//.
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import BarraSuperior from '../componentes/BarraSuperior';
+import BarraLateral from '../componentes/BarraLateral';
+import '../styles/CriarFreguesia.css';
+
+function EnviarTicket() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="container">
+      <BarraSuperior />
+      <div className="corpo">
+        <BarraLateral />
+        <div className="conteudo-criar">
+          <h2>Descrição de Problemers</h2>
+          <hr />
+
+            <label htmlFor="localizacao">Localização</label>
+            <input type="text" id="localizacao" placeholder="Localização" required />
+
+            <label htmlFor="descricao">Descrição</label>
+            <textarea id="descricao" placeholder="Descrição" rows="5" required></textarea>
+
+            <button type="submit" className="btn-enviar">Enviar</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default EnviarTicket;
