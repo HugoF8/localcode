afe2cc589f0ca74fc9a7e1d1f6e4a126f959aba8
renatovb1 <ren.edgar.2001@gmail.com>
2025-04-30 18:22:57 +0100

    zonaPublicacao e adicao de uma pasta react publicacoes

---
+++ frontend/src/componentes/publicacoes/ZonaPublicacoesHome.jsx
@@ -0,0 +1,21 @@
+  function ZonaPublicacoesHome() {
+    return (
+      <div class="zona-publicacoes">
+        <div class="publicacoes-container">
+          <div class="info-perfil">
+            <div class="foto-freguesia">
+
+            </div>
+            <div class="nome-freguesia">
+              <p>SEQUEADE</p>
+            </div>
+          </div>
+          <div class="publicação-utilizador-container">
+          
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
+  export default ZonaPublicacoesHome
