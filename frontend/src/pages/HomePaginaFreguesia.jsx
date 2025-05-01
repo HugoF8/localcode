9bc4d6fea088fe19ed40fe7caa20a2db9b46774d
renatovb1 <ren.edgar.2001@gmail.com>
2025-05-01 17:47:00 +0100

    Home pagina freguesia

--- frontend/src/pages/HomePaginaFreguesia.jsx
+++ frontend/src/pages/HomePaginaFreguesia.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../styles/Home.css';
 import BarraSuperior from '../componentes/BarraSuperior';
 import BarraLateral from '../componentes/BarraLateral';
@@ -5,6 +7,43 @@ import BotaoSeguir from '../componentes/PaginaFreguesia/Seguir';
 
 
 function HomeFreguesia() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      try {
+        const token = localStorage.getItem('token');
+
+        const response = await fetch(`http://localhost:3000/api/posts?id_pagina=${idPagina}`, {
+          method: 'GET',
+          headers: {
+            'Authorization': `Bearer ${token}`
+          }
+        });
+
+        if (!response.ok) throw new Error('Erro ao carregar posts');
+
+        const resultado = await response.json();
+        setPosts(resultado); 
+
+      } catch (error) {
+        console.error('Erro ao procurar posts:', error);
+        setError('Falha ao carregar posts. Tente novamente mais tarde.');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchPosts();
+  }, [idPagina]);
+
+  if (loading) return <p>Carregando posts...</p>;
+  if (error) return <p>{error}</p>;
+
   return (
     <div className="container">
       <BarraSuperior />
