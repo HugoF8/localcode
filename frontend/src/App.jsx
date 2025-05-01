b3f9d57d847190c67cade1c138874154d5de2b2b
renatovb1 <ren.edgar.2001@gmail.com>
2025-05-01 15:39:43 +0100

    definir primeira pagina

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import CriarFreguesia from './pages/CriarFreguesia';
-import EditarPedido from './pages/EditarPedido';
-import Pedidos from './pages/Pedidos';
 import PublicacoesUtilizador from './pages/PublicacoesUtilizador';
 import TicketsUtilizador from './pages/TicketsUtilizador';
 import AprovacoesTickets from './pages/AprovacoesTickets';
-import AprovacoesPublicacoes from './pages/AprovacoesPublicacoes';
 import Login from './pages/Login';
 import Registar1 from './pages/Registar1';
 import Registar2 from './pages/Registar2';
@@ -19,15 +16,12 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/criar-freguesia" element={<CriarFreguesia />} />
-        <Route path="/editar-pedido/:id" element={<EditarPedido />} />
-        <Route path="/pedidos" element={<Pedidos/>} />
         <Route path="/publicacoes-utilizador" element={<PublicacoesUtilizador />} />
         <Route path="/tickets-utilizador" element={<TicketsUtilizador />} />
         <Route path="/AprovacoesTickets" element={<AprovacoesTickets />} />
-        <Route path="/AprovacoesPublicacoes" element={<AprovacoesPublicacoes />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/registar1" element={<Registar1 />} />
         <Route path="/registar2" element={<Registar2 />} />
         <Route path="/enviar-ticket" element={<EnviarTicket />} />
